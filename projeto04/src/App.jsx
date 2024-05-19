06c16852886dc22747c05760498a5b2f7c8e52a6
kauã oraboni <70423924+Orabonii@users.noreply.github.com>
2024-05-19 17:59:31 -0300

    Add files via upload

---
+++ projeto04/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'; 
+
+import ConversorDeMoedas from './Components/Projeto04/Projeto04'; 
+import './App.css'
+  
+
+function App() { 
+
+  return ( 
+
+    <div className="App"> 
+
+      <ConversorDeMoedas/> 
+
+    </div> 
+
+  ); 
+
+} 
+
+export default App; 
